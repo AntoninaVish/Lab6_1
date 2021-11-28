6fbbfde9819af81b955c7a2e91897194270d038c
AntoninaVish <vishivtonya@gmail.com>
2021-11-28 20:45:49 +0300

    Добавьте файлы проекта.

---
+++ Lab6_1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Нахождение самого длинного слова в строке.");
+            Console.WriteLine("Введите с клавиатуры предложение:");
+            string startString = Console.ReadLine();
+            string[] stringArray = startString.Split();
+            string max = string.Empty;
+                      
+            foreach (string s in stringArray)
+            {
+                if (s.Length>max.Length)
+                max = s;
+                
+            }
+            Console.WriteLine(max);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Lab6_2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Определяем является ли строка палиндромом.");
+            Console.WriteLine("Введите с клавиатуры предложение:");
+            string startString = Console.ReadLine();
+            char[] obrString = startString.ToCharArray();
+            Array.Reverse(obrString);
+            string newString = new string(obrString);
+            string[] stringArray = startString.Split();
+            string[] newstringArray = newString.Split();
+            string resultString = "";
+            string finalString = "";
+            foreach (string s in stringArray)
+            {
+                resultString += s.ToLower();
+
+            } 
+            foreach (string s in newstringArray)
+            {
+                finalString += s.ToLower();
+            }
+ 
+
+            if (finalString == resultString)
+            {
+                Console.WriteLine("Палиндром");
+            }
+            else
+            {
+                Console.WriteLine("Не палиндром");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
